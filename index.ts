78e35303c0b957537c5aacbec449f1ae8316f677
ppauel <mail@pauel.eu>
2024-02-10 23:54:10 +0100

    Add ru.json
    community-made translation

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 import { Locale } from 'discord.js';
 import en_us from './en_us.json';
+import ru from './ru.json';
 
 export default {
     [Locale.EnglishUS]: en_us,
+    [Locale.Russian]: ru,
 };
