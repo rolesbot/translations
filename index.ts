091dbea3af045f56e5f240d18f218ebae3231066
ppauel <mail@pauel.eu>
2024-02-11 14:10:40 +0100

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,8 +1,10 @@
 import { Locale } from 'discord.js';
 import en_us from './en_us.json';
+import fr from './fr.json';
 import ru from './ru.json';
 
 export default {
     [Locale.EnglishUS]: en_us,
+    [Locale.French]: fr,
     [Locale.Russian]: ru,
 };
